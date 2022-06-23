dacf1006851285b93b481d8ec4b9b849e2f919c6
NATHANIEL CROSBY <49811392+NATHANIELCROSBY1@users.noreply.github.com>
2022-06-23 10:13:43 -0400

    Initialize return-error-to-user-from-ApplicationCore

---
+++ src/ApplicationCore/Exceptions/BasketLogicExceptions/BasketLogicException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Exceptions.BasketLogicExceptions
+{
+    public class BasketLogicException : Exception
+    {
+        public BasketLogicException()
+            : base($"A {nameof(BasketLogicException)} was thrown.")
+        {
+        }
+
+        protected BasketLogicException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+
+        public BasketLogicException(string message) : base(message)
+        {
+        }
+
+        public BasketLogicException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/ApplicationCore/Exceptions/GuardExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Exceptions;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Exceptions.BasketLogicExceptions;
+
+namespace Ardalis.GuardClauses
+{
+    public static class BasketGuards
+    {
+        public static void NullBasket(this IGuardClause guardClause, int basketId, Basket basket)
+        {
+            if (basket == null)
+                throw new BasketNotFoundException(basketId);
+        }
+
+        public static void TooManyOfItemInBasket(this IGuardClause guardClause, int maxNumberOfItem, int numberOfItemDesired)
+        {
+            if (numberOfItemDesired > maxNumberOfItem)
+                throw new TooManyOfItemInBasketException(maxNumberOfItem);
+        }
+    }
+}
