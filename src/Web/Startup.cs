88cc1b0f3de5fbd1bdf22ee83eb6ade93ac2ddd6
NATHANIEL CROSBY <49811392+NATHANIELCROSBY1@users.noreply.github.com>
2022-06-23 10:13:34 -0400

    Initial commit

---
+++ src/Web/Startup.cs
@@ -0,0 +1,176 @@
+﻿using Ardalis.ListStartupServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.Infrastructure.Data;
+using Microsoft.eShopWeb.Infrastructure.Identity;
+using Microsoft.eShopWeb.Infrastructure.Logging;
+using Microsoft.eShopWeb.Infrastructure.Services;
+using Microsoft.eShopWeb.Web.Interfaces;
+using Microsoft.eShopWeb.Web.Services;
+using Microsoft.eShopWeb.Web.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+
+namespace Microsoft.eShopWeb.Web
+{
+    public class Startup
+    {
+        private IServiceCollection _services;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            // use in-memory database
+            ConfigureInMemoryDatabases(services);
+
+            // use real database
+            //ConfigureProductionServices(services);
+        }
+
+        private void ConfigureInMemoryDatabases(IServiceCollection services)
+        {
+            // use in-memory database
+            services.AddDbContext<CatalogContext>(c =>
+                c.UseInMemoryDatabase("Catalog"));
+
+            // Add Identity DbContext
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseInMemoryDatabase("Identity"));
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            // use real database
+            // Requires LocalDB which can be installed with SQL Server Express 2016
+            // https://www.microsoft.com/en-us/download/details.aspx?id=54284
+            services.AddDbContext<CatalogContext>(c =>
+                c.UseSqlServer(Configuration.GetConnectionString("CatalogConnection")));
+
+            // Add Identity DbContext
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureTestingServices(IServiceCollection services)
+        {
+            ConfigureInMemoryDatabases(services);
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureCookieSettings.Configure(services);
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                       .AddDefaultUI()
+                       .AddEntityFrameworkStores<AppIdentityDbContext>()
+                                       .AddDefaultTokenProviders();
+
+            ConfigureCoreServices.Configure(services, Configuration);
+            ConfigureWebServices.Configure(services, Configuration);
+
+            // Add memory cache services
+            services.AddMemoryCache();
+            services.AddRouting(options =>
+            {
+                // Replace the type and the name used to refer to it with your own
+                // IOutboundParameterTransformer implementation
+                options.ConstraintMap["slugify"] = typeof(SlugifyParameterTransformer);
+            });
+            services.AddMvc(options =>
+            {
+                options.Conventions.Add(new RouteTokenTransformerConvention(
+                         new SlugifyParameterTransformer()));
+
+            });
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AuthorizePage("/Basket/Checkout");
+            });
+            services.AddControllersWithViews();
+            services.AddHttpContextAccessor();
+            services.AddHealthChecks();
+            services.Configure<ServiceConfig>(config =>
+            {
+                config.Services = new List<ServiceDescriptor>(services);
+
+                config.Path = "/allservices";
+            });
+
+            _services = services; // used to debug registered services
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = new
+                        {
+                            status = report.Status.ToString(),
+                            errors = report.Entries.Select(e => new
+                            {
+                                key = e.Key,
+                                value = Enum.GetName(typeof(HealthStatus), e.Value.Status)
+                            })
+                        }.ToJson();
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseShowAllServicesMiddleware();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller:slugify=Home}/{action:slugify=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapHealthChecks("home_page_health_check");
+                endpoints.MapHealthChecks("api_health_check");
+            });
+        }
+    }
+}
---
+++ tests/UnitTests/ApplicationCore/Specifications/BasketWithItemsSpecification.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Moq;
+
+namespace Microsoft.eShopWeb.UnitTests
+{
+    public class BasketWithItems
+    {
+        private readonly int _testBasketId = 123;
+        private readonly string _buyerId = "Test buyerId";
+
+        [Fact]
+        public void MatchesBasketWithGivenBasketId()
+        {
+            var spec = new BasketWithItemsSpecification(_testBasketId);
+
+            var result = GetTestBasketCollection()
+                .AsQueryable()
+                .FirstOrDefault(spec.Criterias.FirstOrDefault());
+
+            Assert.NotNull(result);
+            Assert.Equal(_testBasketId, result.Id);
+        }
+
+        [Fact]
+        public void MatchesNoBasketsIfBasketIdNotPresent()
+        {
+            int badBasketId = -1;
+            var spec = new BasketWithItemsSpecification(badBasketId);
+
+            Assert.False(GetTestBasketCollection()
+                .AsQueryable()
+                .Any(spec.Criterias.FirstOrDefault()));
+        }
+
+        [Fact]
+        public void MatchesBasketWithGivenBuyerId()
+        {
+            var spec = new BasketWithItemsSpecification(_buyerId);
+
+            var result = GetTestBasketCollection()
+                .AsQueryable()
+                .FirstOrDefault(spec.Criterias.FirstOrDefault());
+
+            Assert.NotNull(result);
+            Assert.Equal(_buyerId, result.BuyerId);
+        }
+
+        [Fact]
+        public void MatchesNoBasketsIfBuyerIdNotPresent()
+        {
+            string badBuyerId = "badBuyerId";
+            var spec = new BasketWithItemsSpecification(badBuyerId);
+
+            Assert.False(GetTestBasketCollection()
+                .AsQueryable()
+                .Any(spec.Criterias.FirstOrDefault()));
+        }
+
+        public List<Basket> GetTestBasketCollection()
+        {
+            var basket1Mock = new Mock<Basket>(_buyerId);
+            basket1Mock.SetupGet(s => s.Id).Returns(1);
+            var basket2Mock = new Mock<Basket>(_buyerId);
+            basket2Mock.SetupGet(s => s.Id).Returns(2);
+            var basket3Mock = new Mock<Basket>(_buyerId);
+            basket3Mock.SetupGet(s => s.Id).Returns(_testBasketId);
+
+            return new List<Basket>()
+            {
+                basket1Mock.Object,
+                basket2Mock.Object,
+                basket3Mock.Object
+            };
+        }
+    }
+}
